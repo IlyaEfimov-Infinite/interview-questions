192197388e9eaa56dd12dc1cee6c791f88e12b2b
Ilya Efimov <ilya.efimov@infinitesynergy.ru>
2024-07-24 04:15:42 +0300

    feat: initial questions

---
+++ js/tree-depth.js
@@ -0,0 +1,63 @@
+// В этой задаче требуется определить глубину дерева
+
+const tree = {
+  id: 0,
+  children: [
+    {
+      id: 1,
+      children: [
+        {
+          id: 4,
+          children: [
+            
+          ]
+        },
+        {
+          id: 5,
+          children: [
+            {
+              id: 6,
+              children: [
+                {
+                  id: 7,
+                  children: [
+                    
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    },
+    {
+      id: 2,
+      children: [
+        
+      ]
+    },
+    {
+      id: 3,
+      children: [
+        {
+          id: 8,
+          children: [
+            {
+              id: 10,
+              children: [
+                
+              ]
+            }
+          ]
+        },
+        {
+          id: 9,
+          children: [
+            
+          ]
+        }
+      ]
+    }
+  ]
+}
+
